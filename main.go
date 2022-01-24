0f18354f3fedb16e9827f2cd5c22d9c1327ed0c8
Théo Catala <theo.catala@ynov.com>
2022-01-24 09:29:18 +0100

    InitializeCurrentWord

--- main.go
+++ main.go
@@ -1,213 +1,212 @@
-package main
+// package main
 
-import (
-	"bufio"
-	"log"
-	"math/rand"
-	"net/http"
-	"os"
-	"text/template"
-	"time"
-)
+// import (
+// 	"bufio"
+// 	"log"
+// 	"math/rand"
+// 	"net/http"
+// 	"os"
+// 	"text/template"
+// 	"time"
+// )
 
-// ┌────────────────────────────────────────────────────────────┐
-// │ Globals				             						│
-// └────────────────────────────────────────────────────────────┘
+// // ┌────────────────────────────────────────────────────────────┐
+// // │ Globals				             						│
+// // └────────────────────────────────────────────────────────────┘
 
-var state State // Sauvegarde l'état du jeu global (permet de garder les données entre les requêtes)
+// var state State // Sauvegarde l'état du jeu global (permet de garder les données entre les requêtes)
 
-// ┌────────────────────────────────────────────────────────────┐
-// │ Structs				             						│
-// └────────────────────────────────────────────────────────────┘
-// Représentation de l'état d'une lettre dans le jeu :
-// Letter {Value : "a", Used : false} => La lettre a n'a pas été jouée.
-// Letter {Value : "b", Used : true} => La lettre b a été jouée.
+// // ┌────────────────────────────────────────────────────────────┐
+// // │ Structs				             						│
+// // └────────────────────────────────────────────────────────────┘
+// // Représentation de l'état d'une lettre dans le jeu :
+// // Letter {Value : "a", Used : false} => La lettre a n'a pas été jouée.
+// // Letter {Value : "b", Used : true} => La lettre b a été jouée.
 
-type Letter struct {
-	Value string
-	Used  bool
-}
+// type Letter struct {
+// 	Value string
+// 	Used  bool
+// }
 
-// State représente l'état global du jeu  :
-type State struct {
-	CompleteWord string   // C'est la solution du pendu en clair : Exemple :
-	Letters      []Letter // Liste des 26 lettres
-	Errors       int      // Nb d'erreurs
-	CurrentWord  []string // état du mot en cours de partie : Exemple :
-	GameOver     string   // Msg de fin de partie modifié selon l'état du jeu : "" (en cours), gagné, perdu
-}
+// // State représente l'état global du jeu  :
+// type State struct {
+// 	CompleteWord string   // C'est la solution du pendu en clair : Exemple :
+// 	Letters      []Letter // Liste des 26 lettres
+// 	Errors       int      // Nb d'erreurs
+// 	CurrentWord  []string // état du mot en cours de partie : Exemple :
+// 	GameOver     string   // Msg de fin de partie modifié selon l'état du jeu : "" (en cours), gagné, perdu
+// }
 
-// ┌────────────────────────────────────────────────────────────┐
-// │ Route handlers			             						│
-// └────────────────────────────────────────────────────────────┘
+// // ┌────────────────────────────────────────────────────────────┐
+// // │ Route handlers			             						│
+// // └────────────────────────────────────────────────────────────┘
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	page := template.Must(template.ParseFiles("views/index.html"))
-	page.Execute(w, state)
-}
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+// 	page := template.Must(template.ParseFiles("views/index.html"))
+// 	page.Execute(w, state)
+// }
 
-func playHandler(w http.ResponseWriter, r *http.Request) {
-	page := template.Must(template.ParseFiles("views/game.html"))
+// func playHandler(w http.ResponseWriter, r *http.Request) {
+// 	page := template.Must(template.ParseFiles("views/game.html"))
 
-	// ┌────────────────────────────────┐
-	// │ Initiliaze the game			│
-	// └────────────────────────────────┘
-	switch r.Method {
-	// On met en place ce qui va nous servir pour le jeu
-	case http.MethodGet:
-		word := getNewWord()                            // mot choisi aléatoirement pour le jeu
-		state.CompleteWord = word                       // On sauvegarde le mot complet dans l'état global
-		state.Letters = initializeLetters()             // On sauvegarde la liste des lettres non utilisées dans l'état global
-		state.Errors = 0                                // On sauvegarde le nombre d'erreur dans l'état global
-		state.CurrentWord = initializeCurrentWord(word) // On sauvegarde la version "cryptée" du mot dans l'état global
-		state.GameOver = ""                             // On sauvegarde le message de fin de jeu dans l'état global
-		page.Execute(w, state)
+// 	// ┌────────────────────────────────┐
+// 	// │ Initiliaze the game			│
+// 	// └────────────────────────────────┘
+// 	switch r.Method {
+// 	// On met en place ce qui va nous servir pour le jeu
+// 	case http.MethodGet:
+// 		word := getNewWord()                            // mot choisi aléatoirement pour le jeu
+// 		state.CompleteWord = word                       // On sauvegarde le mot complet dans l'état global
+// 		state.Letters = initializeLetters()             // On sauvegarde la liste des lettres non utilisées dans l'état global
+// 		state.Errors = 0                                // On sauvegarde le nombre d'erreur dans l'état global
+// 		state.CurrentWord = initializeCurrentWord(word) // On sauvegarde la version "cryptée" du mot dans l'état global
+// 		state.GameOver = ""                             // On sauvegarde le message de fin de jeu dans l'état global
+// 		page.Execute(w, state)
 
-	// ┌────────────────────────────────┐
-	// │ Read the letter sent by player │
-	// └────────────────────────────────┘
-	case http.MethodPost:
-		r.ParseForm()
-		letter := r.FormValue("letter")
-		isError := true
+// 	// ┌────────────────────────────────┐
+// 	// │ Read the letter sent by player │
+// 	// └────────────────────────────────┘
+// 	case http.MethodPost:
+// 		r.ParseForm()
+// 		letter := r.FormValue("letter")
+// 		isError := true
 
-		// Replace "_" with the letter from the player, if found
-		for i, v := range state.CompleteWord {
-			if string(v) == letter {
-				isError = false
-				state.CurrentWord[i] = letter
-			}
-			// If all letters from the word have been checked, the letter has not been found isError stays "true"
-		}
-		// Je désactive la lettre qui a été jouée en parcourant mon tableau d'alphabet
-		for i, v := range state.Letters {
-			if v.Value == letter {
-				state.Letters[i] = Letter{Value: v.Value, Used: true}
-				break
-			}
-		}
+// 		// Replace "_" with the letter from the player, if found
+// 		for i, v := range state.CompleteWord {
+// 			if string(v) == letter {
+// 				isError = false
+// 				state.CurrentWord[i] = letter
+// 			}
+// 			// If all letters from the word have been checked, the letter has not been found isError stays "true"
+// 		}
+// 		// Je désactive la lettre qui a été jouée en parcourant mon tableau d'alphabet
+// 		for i, v := range state.Letters {
+// 			if v.Value == letter {
+// 				state.Letters[i] = Letter{Value: v.Value, Used: true}
+// 				break
+// 			}
+// 		}
 
-		if isError {
-			state.Errors++
-		}
+// 		if isError {
+// 			state.Errors++
+// 		}
 
-		// quand on appelle la fonction, on appelle ce qu'elle renvoie... ;)
-		switch isGameOver(state.CurrentWord, state.Errors) {
-		case 2:
-			for i, v := range state.Letters {
-				state.Letters[i] = Letter{Value: v.Value, Used: true}
-			}
-			state.GameOver = "You lose! Game over"
-			state.CurrentWord = getCompleteWord(state.CompleteWord)
+// 		// quand on appelle la fonction, on appelle ce qu'elle renvoie... ;)
+// 		switch isGameOver(state.CurrentWord, state.Errors) {
+// 		case 2:
+// 			for i, v := range state.Letters {
+// 				state.Letters[i] = Letter{Value: v.Value, Used: true}
+// 			}
+// 			state.GameOver = "You lose! Game over"
+// 			state.CurrentWord = getCompleteWord(state.CompleteWord)
 
-		case 1:
-			for i, v := range state.Letters {
-				state.Letters[i] = Letter{Value: v.Value, Used: true}
-			}
-			state.GameOver = "You win! Game over"
-		}
+// 		case 1:
+// 			for i, v := range state.Letters {
+// 				state.Letters[i] = Letter{Value: v.Value, Used: true}
+// 			}
+// 			state.GameOver = "You win! Game over"
+// 		}
 
-		page.Execute(w, state)
-	}
-}
+// 		page.Execute(w, state)
+// 	}
+// }
 
-// ┌────────────────────────────────────────────────────────────┐
-// │ Utilities				             						│
-// └────────────────────────────────────────────────────────────┘
+// // ┌────────────────────────────────────────────────────────────┐
+// // │ Utilities				             						│
+// // └────────────────────────────────────────────────────────────┘
 
-/*
-Récupère une ligne aléatoirement dans le fichier en .txt
+// /*
+// Récupère une ligne aléatoirement dans le fichier en .txt
 
-Exemple :
-"carotte"
-*/
-func getNewWord() string {
-	words, err := os.Open("./files/words.txt")
-	if err != nil {
-		log.Fatal("y'a r", err)
-	}
-	motsDuFichier := bufio.NewScanner(words)
-	var mots []string
-	for motsDuFichier.Scan() {
-		mots = append(mots, motsDuFichier.Text())
-	}
-	rand.Seed(time.Now().UTC().UnixNano())
-	chiffre := rand.Intn(len(mots))
-	return (mots[chiffre])
-}
+// Exemple :
+// "carotte"
+// */
+// func getNewWord() string {
+// 	words, err := os.Open("./files/words.txt")
+// 	if err != nil {
+// 		log.Fatal("y'a r", err)
+// 	}
+// 	motsDuFichier := bufio.NewScanner(words)
+// 	var mots []string
+// 	for motsDuFichier.Scan() {
+// 		mots = append(mots, motsDuFichier.Text())
+// 	}
+// 	rand.Seed(time.Now().UTC().UnixNano())
+// 	chiffre := rand.Intn(len(mots))
+// 	return (mots[chiffre])
+// }
 
-/*
-Génère une liste de 26 lettres non utilisées.
+// /*
+// Génère une liste de 26 lettres non utilisées.
 
-Exemple :
-[
-	{Value : "a", Used: false},
-	{Value : "b", Used: false},
-	...,
-	{Value : "z", Used: false}
-]
-*/
-func initializeLetters() []Letter {}
+// Exemple :
+// [
+// 	{Value : "a", Used: false},
+// 	{Value : "b", Used: false},
+// 	...,
+// 	{Value : "z", Used: false}
+// ]
+// */
+// func initializeLetters() []Letter {}
 
-/*
-Génère les _ correspondants au mot /!\ Attention si espaces
+// /*
+// Génère les _ correspondants au mot /!\ Attention si espaces
 
-Exemple :
-word = Fifa
-return => _ _ _ _
-*/
-func initializeCurrentWord(word string) []string {
-}
+// Exemple :
+// word = Fifa
+// return => _ _ _ _
+// */
+// func initializeCurrentWord(word string) []string {}
 
-/*
-Renvoie le mot sous forme d'un tableau de string
+// /*
+// Renvoie le mot sous forme d'un tableau de string
 
-Exemple :
-word = fifa
-return => ["f","i","f","a"]
-*/
-func getCompleteWord(word string) []string {}
+// Exemple :
+// word = fifa
+// return => ["f","i","f","a"]
+// */
+// func getCompleteWord(word string) []string {}
 
-/*
-Renvoie 0 si partie en cours, 1 si c'est gagné, 2 si perdu
+// /*
+// Renvoie 0 si partie en cours, 1 si c'est gagné, 2 si perdu
 
-Exemple :
-word = ["f","_","f","a"]
-errors = 3
-return 0
+// Exemple :
+// word = ["f","_","f","a"]
+// errors = 3
+// return 0
 
-Exemple 2 :
-word = ["f","_","f","a"]
-errors = 6
-return 2
+// Exemple 2 :
+// word = ["f","_","f","a"]
+// errors = 6
+// return 2
 
-Exemple 3 :
-word = ["f","i","f","a"]
-errors = 2
-return 1
-*/
-func isGameOver(word []string, errors int) int {}
+// Exemple 3 :
+// word = ["f","i","f","a"]
+// errors = 2
+// return 1
+// */
+// func isGameOver(word []string, errors int) int {}
 
-// ┌────────────────────────────────────────────────────────────┐
-// │ Main					             						│
-// └────────────────────────────────────────────────────────────┘
+// // ┌────────────────────────────────────────────────────────────┐
+// // │ Main					             						│
+// // └────────────────────────────────────────────────────────────┘
 
-func main() {
+// func main() {
 
-	// ┌────────────────────────────────┐
-	// │ Serve static files				│
-	// └────────────────────────────────┘
-	fileserver := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static", fileserver))
+// 	// ┌────────────────────────────────┐
+// 	// │ Serve static files				│
+// 	// └────────────────────────────────┘
+// 	fileserver := http.FileServer(http.Dir("./static"))
+// 	http.Handle("/static/", http.StripPrefix("/static", fileserver))
 
-	// ┌────────────────────────────────┐
-	// │ Routes							│
-	// └────────────────────────────────┘
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/play", playHandler)
+// 	// ┌────────────────────────────────┐
+// 	// │ Routes							│
+// 	// └────────────────────────────────┘
+// 	http.HandleFunc("/", homeHandler)
+// 	http.HandleFunc("/play", playHandler)
 
-	// ┌────────────────────────────────┐
-	// │ Start the server				│
-	// └────────────────────────────────┘
-	http.ListenAndServe(":8080", nil)
-}
+// 	// ┌────────────────────────────────┐
+// 	// │ Start the server				│
+// 	// └────────────────────────────────┘
+// 	http.ListenAndServe(":8080", nil)
+// }
--- zdsf.go
+++ zdsf.go
@@ -1,25 +1,57 @@
-// package main
+package main
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"log"
-// 	"math/rand"
-// 	"os"
-// 	"time"
-// )
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
 
-// func main() {
-// 	words, err := os.Open("./files/words.txt")
-// 	if err != nil {
-// 		log.Fatal("y'a r", err)
-// 	}
-// 	motsDuFichier := bufio.NewScanner(words)
-// 	var mots []string
-// 	for motsDuFichier.Scan() {
-// 		mots = append(mots, motsDuFichier.Text())
-// 	}
-// 	rand.Seed(time.Now().UTC().UnixNano())
-// 	chiffre := rand.Intn(len(mots))
-// 	fmt.Println(mots[chiffre])
-// }
+var motString []string
+
+func main() {
+	c := getCompleteWord()
+	initializeLetters(c)
+}
+
+func getNewWord() string {
+	words, err := os.Open("./files/words.txt")
+	if err != nil {
+		log.Fatal("y'a r", err)
+	}
+	motsDuFichier := bufio.NewScanner(words)
+	var mots []string
+	for motsDuFichier.Scan() {
+		mots = append(mots, motsDuFichier.Text())
+	}
+	rand.Seed(time.Now().UTC().UnixNano())
+	chiffre := rand.Intn(len(mots))
+	return (mots[chiffre])
+}
+
+func getCompleteWord() []string {
+	mot := []rune(getNewWord())
+	var motString []string
+	for i := 0; i < len(mot); i++ {
+		var conver string
+		conver = string(mot[i] - 32)
+		motString = append(motString, conver)
+	}
+	return motString
+}
+func initializeLetters(words []string) {
+	var tabString []string
+	for i := 0; i < len(words); i++ {
+		tabString = append(tabString, "_ ")
+	}
+
+	// for i := 0; i < len(tab); i++ {
+	// 	tabString[tab[i]] = words[tab[i]]
+	// }
+	fmt.Println(tabString)
+	// for i := 0; i < len(tabString); i++ {
+	// 	fmt.Printf(tabString[i])
+	// }
+}
